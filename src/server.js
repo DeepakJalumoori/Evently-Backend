02d486caa73bfbe47960b0e5dfd59c83aafaeff2
Deepak <deepakjalmoori@gmail.com>
2026-07-06 22:49:16 +0530

    Refactor project structure

--- src/server.js
+++ src/server.js
@@ -1,21 +1,4 @@
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
-
-const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
-app.get("/", (req, res) => {
-  res.send("No Events Found!");
-});
 
 const port = process.env.PORT || 3000;
 
