6d9c1db205032b00f25ab3c7ea441f12975ee2d6
Deepak <deepakjalmoori@gmail.com>
2026-07-08 03:50:35 +0530

    auth Middleware added

---
+++ src/Middlewares/authMiddleware.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization header missing",
+      });
+    }
+
+    if (!authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid authorization format",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
+const authMiddleware = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
+
+//Testing authMiddleware by giving token in the header
+// router.get("/profile", authMiddleware, (req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     user: req.user,
+//   });
+// });
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
