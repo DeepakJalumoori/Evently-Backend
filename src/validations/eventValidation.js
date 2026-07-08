faa5982f31848d5e4fa580227879eea20cd33d92
Deepak <deepakjalmoori@gmail.com>
2026-07-08 06:43:39 +0530

    Event model and Event validations added

---
+++ src/validations/eventValidation.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    capacity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    availableSeats: {
+      type: Number,
+      required: true,
+    },
+
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Event", eventSchema);
